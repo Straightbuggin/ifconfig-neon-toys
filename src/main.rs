f0c358822b921598b17deae760016af7a75420a0
straightbuggin <alex.trei@gmail.com>
2023-10-23 11:05:30 -0700

    fixed error where correct IP address was not being displayed. also reducted data provided

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::{
-    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream}, 
 };
 use ifconfig_neon_toys::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("[::]:8080").unwrap();
+    let tcp_listener = TcpListener::bind("[::]:8080").unwrap();
+    let listener = tcp_listener;
     let pool = ThreadPool::new(100);
 
 
@@ -23,7 +23,6 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-
     let mut headers_html = String::new();
 
     let peer_addr = match stream.peer_addr() {
@@ -34,53 +33,97 @@ fn handle_connection(mut stream: TcpStream) {
         }
     };
 
-    let ip_address = peer_addr.ip();
-
-    let formatted_ip = match ip_address {
-        std::net::IpAddr::V4(v4_addr) => format!("{}", v4_addr),
-        std::net::IpAddr::V6(v6_addr) => match is_ipv4_mapped(v6_addr) {
-            Some(v4_addr) => format!("{}", v4_addr),
-            None => format!("{}", v6_addr),
-        },
-    };
-
     let mut buf_reader = BufReader::new(&mut stream);
 
+    // Variables to store the specified headers and other information
+    let mut method = String::new();
+    let mut user_agent = String::new();
+    let mut language = String::new();
+    let mut referer = String::new();
+    let mut encoding = String::new();
+    let mut mime_type = String::new();
+    let mut charset = String::new();
+    let mut x_forwarded_for = String::new();
+    let mut keep_alive = String::new();
 
-    headers_html.push_str(&format!("<tr><td>IP Address</td><td><strong>{}</strong></td></tr>", formatted_ip));
+    // Initial processing to get the method from the request line
+    {
+        let mut request_line = String::new();
+        buf_reader.read_line(&mut request_line).unwrap();
+        method = request_line.split_whitespace().next().unwrap_or("").to_string();
+    }
+
     loop {
         let mut line = String::new();
         let bytes_read = buf_reader.read_line(&mut line).unwrap();
         if bytes_read == 0 || line == "\r\n" {
             break;  // End of headers or connection closed
         }
-       
+
         let parts: Vec<&str> = line.trim().splitn(2, ':').collect();
         if parts.len() == 2 {
-            headers_html.push_str(&format!("<tr><td>{}</td><td>{}</td></tr>", parts[0], parts[1].trim()));
+            let header_name = parts[0].to_lowercase();
+            let header_value = parts[1].trim();
+            match header_name.as_str() {
+                "user-agent" => user_agent = header_value.to_string(),
+                "accept-language" => language = header_value.to_string(),
+                "referer" => referer = header_value.to_string(),
+                "accept-encoding" => encoding = header_value.to_string(),
+                "accept" => mime_type = header_value.to_string(),
+                "accept-charset" => charset = header_value.to_string(),
+                "x-forwarded-for" => x_forwarded_for = header_value.to_string(),
+                "connection" => keep_alive = header_value.to_string(),
+                _ => {},
+            }
         }
     }
 
-    
+    let port = peer_addr.port();
 
+    let ip_address = if !x_forwarded_for.is_empty() {
+        x_forwarded_for.split(',').next().unwrap_or("").trim().to_string()
+    } else {
+        peer_addr.ip().to_string()
+    };
     
+    headers_html.push_str(&format!(
+        "<tr><td>IP Address</td><td><strong>{}</strong></td></tr>\
+        <tr><td>Port</td><td>{}</td></tr>\
+        <tr><td>Method</td><td>{}</td></tr>\
+        <tr><td>User Agent</td><td>{}</td></tr>\
+        <tr><td>Language</td><td>{}</td></tr>\
+        <tr><td>Referer</td><td>{}</td></tr>\
+        <tr><td>Encoding</td><td>{}</td></tr>\
+        <tr><td>MIME Type</td><td>{}</td></tr>\
+        <tr><td>Charset</td><td>{}</td></tr>\
+        <tr><td>X-Forwarded-For</td><td>{}</td></tr>\
+        <tr><td>Keep Alive</td><td>{}</td></tr>",
+        ip_address,
+        port,
+        method,
+        user_agent,
+        language,
+        referer,
+        encoding,
+        mime_type,
+        charset,
+        x_forwarded_for,
+        keep_alive,
+    ));
+
     let status_line = "HTTP/1.1 200 OK";
     let filename = "response.html";
-    
-    let mut contents = fs::read_to_string(filename).unwrap();
-    contents = contents.replace("{headers_table}", &headers_html); 
-    
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}", status_line = status_line, length = length, contents = contents);
-    
-    stream.write_all(response.as_bytes()).unwrap();
-}
 
-fn is_ipv4_mapped(ipv6: std::net::Ipv6Addr) -> Option<std::net::Ipv4Addr> {
-    let octets = ipv6.octets();
-    if &octets[0..10] == [0; 10] && octets[10] == 0xff && octets[11] == 0xff {
-        Some(std::net::Ipv4Addr::new(octets[12], octets[13], octets[14], octets[15]))
-    } else {
-        None
-    }
+    let mut contents = std::fs::read_to_string(filename).unwrap();
+    contents = contents.replace("{headers_table}", &headers_html);
+
+    let length = contents.len();
+    let response = format!(
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}",
+        status_line = status_line,
+        length = length,
+        contents = contents
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
